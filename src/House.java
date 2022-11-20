5a648b02daea3ae2e0cccd18acf122de084474c1
Muhammed <muhammedtoichubai@gmail.com>
2022-11-20 10:44:30 -0600

    added image

---
+++ src/House.java
@@ -0,0 +1,13 @@
+/**
+ * @author Muhammed Asantegin
+ * @created 20/11/2022 - 10:40 AM
+ **/
+public class House {
+    String title;
+
+    public static void main(String[] args) {
+        House house = new House();
+        System.out.println(house.title);
+    }
+
+}
