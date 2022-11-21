57a887c9b75252edc59a3d93cb734aac8171c42a
Muhammed <muhammedtoichubai@gmail.com>
2022-11-20 22:26:47 -0600

    README.md - OOP

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+/**
+ * @author Muhammed Asantegin
+ * @created 20/11/2022 - 9:51 PM
+ **/
+public class Student {
+    private String name; // private = restricted access
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@
  * @author ${USER} Asantegin
  * @created ${DAY}/${MONTH}/${YEAR} - ${TIME}
  **/
-public class Main {
+public class Test {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
